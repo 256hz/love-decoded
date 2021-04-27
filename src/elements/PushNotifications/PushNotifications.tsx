cbe163a505f144de01d203b9c0928a4d507d5867
Michael Figueroa <michael.figueroa@figstudios.io>
2021-04-26 20:19:45 -0700

    fixed es lint issues

--- src/elements/PushNotifications/PushNotifications.tsx
+++ src/elements/PushNotifications/PushNotifications.tsx
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
-import {Notification, Notifications} from 'react-native-notifications';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Component } from 'react';
+import { Notification, Notifications } from 'react-native-notifications';
 
 class MyComponent extends Component {
-  constructor(props) {
-    super(props);
-    Notifications.registerRemoteNotifications();
+	constructor(props) {
+		super(props);
+		Notifications.registerRemoteNotifications();
 
-    Notifications.events().registerNotificationReceivedForeground((notification: Notification, completion) => {
-      console.log(`Notification received in foreground: ${notification.title} : ${notification.body}`);
-      completion({alert: false, sound: false, badge: false});
-    });
+		Notifications.events().registerNotificationReceivedForeground((notification: Notification, completion) => {
+			console.log(`Notification received in foreground: ${notification.title} : ${notification.body}`);
+			completion({ alert: false, sound: false, badge: false });
+		});
 
-    Notifications.events().registerNotificationOpened((notification: Notification, completion) => {
-      console.log(`Notification opened: ${notification.payload}`);
-      completion();
-    });
-  }
+		Notifications.events().registerNotificationOpened((notification: Notification, completion) => {
+			console.log(`Notification opened: ${notification.payload}`);
+			completion();
+		});
+	}
 }
 
