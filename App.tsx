76c581b460c9469b03e3144e322cf359ccd36b7e
Michael Figueroa <michael.figueroa@figstudios.io>
2021-04-29 11:08:19 -0700

    removed azure push notifications

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
+/* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
 // must be before React Native import
 import 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import {
-	Alert,
 	Platform,
 	StatusBar,
 } from 'react-native';
@@ -15,146 +15,16 @@ import getStore from 'redux/getStore';
 import Modals from '@elements/Modals';
 import colors from 'elements/globalStyles/color';
 
-const NotificationHub = require('react-native-azurenotificationhub/index.ios');
-
-const connectionString = 'Endpoint=sb://love-decoded-ns-dev.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=xoIo6GmDcm4R/g98owY5/5m8KPvzOyerrKM4vOgPH08=';       // The Notification Hub connection string
-const hubName = 'love-decoded-notification-hub-dev';
-
-let remoteNotificationsDeviceToken = '';  // The device token registered with APNS
 export const { store, persistor } = getStore();
+
 if (Platform.OS === 'android') {
 	StatusBar.setBackgroundColor(colors.GrayTransparent028);
 	StatusBar.setTranslucent(true);
 }
 export default class App extends Component {
 
-	requestPermissions = () => {
-		// register: Fired when the user registers for remote notifications. The
-		// handler will be invoked with a hex string representing the deviceToken.
-		NotificationHub.addEventListener('register', this._onRegistered);
-
-		// registrationError: Fired when the user fails to register for remote
-		// notifications. Typically occurs when APNS is having issues, or the device
-		// is a simulator. The handler will be invoked with {message: string, code: number, details: any}.
-		NotificationHub.addEventListener('registrationError', this._onRegistrationError);
-
-		// registerAzureNotificationHub: Fired when registration with azure notification hubs successful
-		// with object {success: true}
-		NotificationHub.addEventListener('registerAzureNotificationHub', this._onAzureNotificationHubRegistered);
-
-		// azureNotificationHubRegistrationError: Fired when registration with azure notification hubs
-		// fails with object {message: string, details: any}
-		NotificationHub.addEventListener('azureNotificationHubRegistrationError', this._onAzureNotificationHubRegistrationError);
-
-		// notification: Fired when a remote notification is received. The
-		// handler will be invoked with an instance of `AzureNotificationHubIOS`.
-		NotificationHub.addEventListener('notification', this._onRemoteNotification);
-
-		// localNotification: Fired when a local notification is received. The
-		// handler will be invoked with an instance of `AzureNotificationHubIOS`.
-		NotificationHub.addEventListener('localNotification', this._onLocalNotification);
-
-		// Requests notification permissions from iOS, prompting the user's
-		// dialog box. By default, it will request all notification permissions, but
-		// a subset of these can be requested by passing a map of requested
-		// permissions.
-		// The following permissions are supported:
-		//  - `alert`
-		//  - `badge`
-		//  - `sound`
-		//
-		// returns a promise that will resolve when the user accepts,
-		// rejects, or if the permissions were previously rejected. The promise
-		// resolves to the current state of the permission of
-		// {alert: boolean, badge: boolean,sound: boolean }
-		NotificationHub.requestPermissions()
-			.then(console.log)
-			.catch(console.warn);
-	};
-
-	register = () => {
-		NotificationHub.register(remoteNotificationsDeviceToken, { connectionString, hubName })
-			.then(console.log)
-			.catch(console.warn);
-	};
-
-	unregister = () => {
-		NotificationHub.unregister()
-			.then(console.log)
-			.catch(console.warn);
-	};
-
-	_onRegistered = (deviceToken) => {
-		remoteNotificationsDeviceToken = deviceToken;
-		Alert.alert(
-			'Registered For Remote Push',
-			`Device Token: ${deviceToken}`,
-			[ {
-				text: 'Dismiss',
-				onPress: () => {},
-			} ],
-		);
-	};
-
-	_onRegistrationError = (error) => {
-		Alert.alert(
-			'Failed To Register For Remote Push',
-			`Error (${error.code}): ${error.message}`,
-			[ {
-				text: 'Dismiss',
-				onPress: () => {},
-			} ],
-		);
-	};
-
-	_onRemoteNotification = (notification) => {
-		Alert.alert(
-			'Push Notification Received',
-			`Alert message: ${  notification.getMessage()}`,
-			[ {
-				text: 'Dismiss',
-				onPress: () => {},
-			} ],
-		);
-	};
-
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	_onAzureNotificationHubRegistered = (registrationInfo) => {
-		Alert.alert('Registered For Azure notification hub',
-			'Registered For Azure notification hub',
-			[ {
-				text: 'Dismiss',
-				onPress: () => {},
-			} ],
-		);
-	};
-
-	_onAzureNotificationHubRegistrationError = (error) => {
-		Alert.alert(
-			'Failed To Register For Azure Notification Hub',
-			`Error (${error.code}): ${error.message}`,
-			[ {
-				text: 'Dismiss',
-				onPress: () => {},
-			} ],
-		);
-	};
-
-	_onLocalNotification = (notification) => {
-		// Note notification will be object for iOS
-		Alert.alert(
-			'Local Notification Received',
-			`Alert message: ${  notification.getMessage()}`,
-			[ {
-				text: 'Dismiss',
-				onPress: () => {},
-			} ],
-		);
-	};
-
 	render() {
-		this.requestPermissions();
-		this.register();
+
 		return (
 			<Provider store={store}>
 				<PersistGate loading={null} persistor={persistor()}>
