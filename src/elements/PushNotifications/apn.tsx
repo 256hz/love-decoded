d13043ccb46cd2fb9ede7ca13265906c57330310
Michael Figueroa <michael.figueroa@figstudios.io>
2021-04-29 10:45:13 -0700

    implemented react-native-push-notifications

---
+++ src/elements/PushNotifications/apn.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import PushNotificationIOS from '@react-native-community/push-notification-ios';
+import PushNotification from 'react-native-push-notification';
+
+const configure = () => {
+	PushNotification.configure({
+		// user accepted notification permission - register token
+		onRegister(tokenData) {
+			const { token } = tokenData;
+
+			// handle device token
+			// send token to server...
+			// store in AsyncStorage...
+		},
+		// notification received / opened in-app event
+		onNotification(notification) {
+			notification.finish(PushNotificationIOS.FetchResult.NoData);
+		},
+		// outlining what permissions to accept
+		permissions: {
+			alert: true,
+			badge: true,
+			sound: true,
+		},
+	});
+};
